16ce71a0f5adf2e1cd4f2bf8f6eca363399c1361
Dylan Hand <dylan.hand@gmail.com>
2026-04-11 17:32:11 +0200

    Shorter sqlite cache ttl to prevent showing stale badge data

--- server/cache/cache-manager.ts
+++ server/cache/cache-manager.ts
@@ -234,7 +234,7 @@ export class CacheManager {
 
       // Cache all pipelines from this org with 15-minute TTL
       for (const pipeline of allOrgPipelines) {
-        this.db.cachePipeline(pipeline.slug, orgSlug, pipeline, 15 * 60) // 15 minutes
+        this.db.cachePipeline(pipeline.slug, orgSlug, pipeline, 60) // 1 minute
       }
 
       allPipelines.push(...allOrgPipelines)
